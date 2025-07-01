a08e06d59114e6bc4791c8803995ebe917397c07
Colton Baum <coltonbaum12@gmail.com>
2025-07-01 17:56:12 -0600

    Add stub Alembic revision 2d7ffad83a58

---
+++ migrations/versions/2d7ffad83a58_stub_for_missing_alembic_rev.py
@@ -0,0 +1,24 @@
+"""stub for missing Alembic rev
+
+Revision ID: 2d7ffad83a58
+Revises: 8bac7ba2c203
+Create Date: 2025-07-01 17:55:46.256348
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2d7ffad83a58'
+down_revision = '8bac7ba2c203'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
