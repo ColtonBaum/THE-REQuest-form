26534b260ff5678b03aed02fa72ce692aadfd9ae
Colton Baum <coltonbaum12@gmail.com>
2025-07-02 09:34:36 -0600

    Point merge revision at both heads

--- migrations/versions/7eb72a99f019_merge_notes_main_heads.py
+++ migrations/versions/7eb72a99f019_merge_notes_main_heads.py
@@ -1,13 +1,11 @@
 """Merge notes & main heads
 
 Revision ID: 7eb72a99f019
-Revises: 2d7ffad83a58
+Revises: 8bac7ba2c203, 2d7ffad83a58
 Create Date: 2025-07-02 09:20:51.782568
-
 """
 from alembic import op
 import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision = '7eb72a99f019'
@@ -15,10 +13,9 @@ down_revision = ('8bac7ba2c203', '2d7ffad83a58')
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
+    # nothing to do; this is pure merge
     pass
-
 
 def downgrade():
     pass
