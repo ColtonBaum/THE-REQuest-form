2209d2dfe65717d346385023e3658a54f469e008
Colton Baum <coltonbaum12@gmail.com>
2025-07-02 09:49:52 -0600

    Chain stub migration onto add_notes head

--- migrations/versions/2d7ffad83a58_stub_for_missing_alembic_rev.py
+++ migrations/versions/2d7ffad83a58_stub_for_missing_alembic_rev.py
@@ -9,16 +9,18 @@ from alembic import op
 import sqlalchemy as sa
 
 
+# … in 2d7ffad83a58_stub_for_missing_alembic_rev.py …
+
 # revision identifiers, used by Alembic.
 revision = '2d7ffad83a58'
+# chain *after* the add_notes migration
 down_revision = '8bac7ba2c203'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
+    # stub: nothing to do
     pass
-
 
 def downgrade():
     pass
